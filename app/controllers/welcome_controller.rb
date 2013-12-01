fe8907bd7e424996cb0e8ae4b3d8aec7bbbd1113
sai <sai@sai-300E4Z-300E5Z-300E7Z.(none)>
2013-12-01 19:49:06 +0530

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+  def index
+
+@u = Time.now
+
+
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'ba58bc9dc3ef87d382c49f4fcd00e744e5f1d83dbd109b0f318b3e6e327bef84a21c44cf892f992d47f2d7d19d247a6117dd20bb22d14655326c88b896f34742'
